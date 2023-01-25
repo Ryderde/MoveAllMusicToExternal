c93b9befb3c0b2d1d1fc1d7cf1d21921ff7bba18
Rydede <felixaburaka@gmail.com>
2023-01-25 07:09:30 +0300

    Basic Functionalities Added

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-#
+#! /usr/bin/python
+
+import os
+from pathlib import Path
+
+os.chdir("/storage/emulated/0")
 
 
+def is_audio(file):
+    p= Path(file)
+    if p.suffix == ".mp3" or p.suffix ==".m4a":
+        return True
+
+for dirPath, dirName, fileNames in os.walk(os.getcwd()):
+    for fileName in fileNames:
+        if is_audio(fileName):
+            print(f"{dirPath}  =>  {fileName}")
